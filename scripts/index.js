26424b3fafa9757e603cdd9a7f9f629910b7d64b
Maggie.ctrl <margaretkariuki742@gmail.com>
2025-01-26 15:12:55 +0300

    Commiting JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 //Akan arrays
+ const maleNames = [
+     "Kwasi", "Kwadwo", "Kwabena" , "Kwaku",   "Yaw",   "Kofi",
+     
+     
+     
+   ];
+   const femaleNames = [
+     "Akosua",
+     "Adwoa",
+     "Abenaa",
+     "Akua",
+     "Yaa",
+     "Afua",
+     "Ama",
