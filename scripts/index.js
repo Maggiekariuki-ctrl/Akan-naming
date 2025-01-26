7f1fd7e998e2f9b16fd1257463347c74ef796585
Maggie.ctrl <margaretkariuki742@gmail.com>
2025-01-26 23:01:44 +0300

    adding formula

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,5 @@ function getAkanName(day, month, year) {
     // Extract century (CC) and year (YY)
     const CC = Math.floor(year / 100);
     const YY = year%100;
+ //formula to calculate 
+  const result = (((CC / 4) - 2 * CC - 1) + ((5 * YY) / 4) + ((26 * (month + 1)) / 10) + day) % 7;
