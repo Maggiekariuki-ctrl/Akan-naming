7b033884f1d9d8287b74739e2c9f3085be73d66c
Maggie.ctrl <margaretkariuki742@gmail.com>
2025-01-26 23:04:41 +0300

    To return femake name

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,6 @@ function getAkanName(day, month, year) {
     const YY = year%100;
  //formula to calculate 
   const result = (((CC / 4) - 2 * CC - 1) + ((5 * YY) / 4) + ((26 * (month + 1)) / 10) + day) % 7;
+const isFemale = result % 2 === 0; // Even results are female names, odd are male
+    return isFemale ? akanNames[result][0] : akanNames[result][1];
+}
